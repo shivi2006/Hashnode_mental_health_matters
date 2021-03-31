4a67782e9c3beba59431d44be63e6dd6ab388597
Shivi Mittal <54214320+shivi2006@users.noreply.github.com>
2021-03-31 18:15:35 +0530

    Add files via upload

---
+++ dummy_doctors.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'Doctor.dart';
+
+
+
+ var dummy = [
+  Doctor(
+    id: 'c1',
+    title: 'Dr.Annie',
+    image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedicalxpress.com%2Fnews%2F2021-01-covid-doctors-online-death-threats.html&psig=AOvVaw3GUQ2m001OQhESvzdBDBbr&ust=1617274171068000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMi655q02u8CFQAAAAAdAAAAABA5'
+  ),
+  Doctor(
+    id: 'c2',
+    title: 'Dr.Riya',
+    image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.usatoday.com%2Fstory%2Fnews%2Fhealth%2F2020%2F06%2F26%2Fu-s-doctor-shortage-worsens-especially-black-and-latino-groups%2F3262561001%2F&psig=AOvVaw3GUQ2m001OQhESvzdBDBbr&ust=1617274171068000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMi655q02u8CFQAAAAAdAAAAABAt'
+    
+  ),
+  Doctor(
+    id: 'c3',
+    title: 'Dr.Frank',
+    image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fc%2FDoctorMike&psig=AOvVaw3GUQ2m001OQhESvzdBDBbr&ust=1617274171068000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMi655q02u8CFQAAAAAdAAAAABA_'
+  
+  ),
+   Doctor(
+    id: 'c3',
+    title: 'Dr.Mike',
+    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fblack-doctor&psig=AOvVaw3GUQ2m001OQhESvzdBDBbr&ust=1617274171068000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMi655q02u8CFQAAAAAdAAAAABAn',
+  
+  ),
+   Doctor(
+    id: 'c3',
+    title: 'Dr.Harvey',
+    image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cdc.gov%2Fdiabetes%2Flibrary%2Fspotlights%2Fno-doctor.html&psig=AOvVaw3GUQ2m001OQhESvzdBDBbr&ust=1617274171068000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMi655q02u8CFQAAAAAdAAAAABBX'
+  
+  ),
+   Doctor(
+    id: 'c3',
+    title: 'Dr.Lauren',
+     image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mdlinx.com%2Farticle%2Fthese-doctor-specialties-are-the-happiest%2F7m8muP4vVSc1nSLq4WX706&psig=AOvVaw3GUQ2m001OQhESvzdBDBbr&ust=1617274171068000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMi655q02u8CFQAAAAAdAAAAABBd'
+  
+  ),
+   Doctor(
+    id: 'c3',
+    title: 'Dr.Shawn',
+    image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hcplive.com%2Fview%2Fhow-to-spot-a-bad-doctor&psig=AOvVaw3GUQ2m001OQhESvzdBDBbr&ust=1617274171068000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMi655q02u8CFQAAAAAdAAAAABBj'
+  
+  ),
+  
+];

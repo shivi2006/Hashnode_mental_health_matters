e99050d57a479ce0f28e5c603f5b736075ff530e
Shivi Mittal <54214320+shivi2006@users.noreply.github.com>
2021-03-31 18:15:07 +0530

    Add files via upload

---
+++ lib/Doctor.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Doctor{
+  String id;
+  String title;
+  String image;
+
+
+  Doctor({
+    @required this.id,
+    @required this.title,
+    @required this.image,
+  });
+}
+
+
